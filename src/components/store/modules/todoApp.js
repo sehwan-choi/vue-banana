09e81ea6dd100828cc67e89c0cbc959b9d8a128a
sehwan <nu940907@gmail.com>
2023-09-27 21:40:17 +0900

    프로젝트 구조화와 모듈화

---
+++ src/components/store/modules/todoApp.js
@@ -0,0 +1,58 @@
+const storage = {
+    fetch() {
+        const arr = [];
+        if (localStorage.length > 0) {
+            for (let i = 0; i < localStorage.length; i++) {
+                const item = localStorage.getItem(localStorage.key(i));
+                const parse = JSON.parse(item);
+                arr.push(parse);
+            }
+        }
+        console.log(arr);
+        return arr;
+    },
+}
+
+export default {
+    state: {
+        todoItems: storage.fetch(),
+            price: 100
+        },
+    getters: {
+        storedTodoItems(state) {
+            return state.todoItems;
+        },
+        originalPrice(state) {
+            return state.price;
+        },
+        doublePrice(state) {
+            return state.price * 2;
+        },
+        triplePrice(state) {
+            return state.price * 3;
+        }
+    },
+    mutations: {
+        addOneItem(state, newTodoItem) {
+            const obj = { completed: false, item: newTodoItem };
+            localStorage.setItem(newTodoItem, JSON.stringify(obj));
+            state.todoItems.push(obj);
+        },
+        removeOneItem(state, payload) {
+            console.log('removeOneItem',payload);
+            localStorage.removeItem(payload.item);
+            state.todoItems.splice(payload.index, 1);
+        },
+        removeAll(state) {
+            localStorage.clear();
+            state.todoItems = [];
+        },
+        complete(state, payload) {
+            console.log(payload);
+            
+            state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
+            localStorage.removeItem(payload.todoItem);
+            localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
+        },
+    },
+}
--- src/components/store/store.js
+++ src/components/store/store.js
@@ -1,63 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import todoApp from './modules/todoApp';
 
 Vue.use(Vuex);
 
-const storage = {
-    fetch() {
-        const arr = [];
-        if (localStorage.length > 0) {
-            for (let i = 0; i < localStorage.length; i++) {
-                const item = localStorage.getItem(localStorage.key(i));
-                const parse = JSON.parse(item);
-                arr.push(parse);
-            }
-        }
-        console.log(arr);
-        return arr;
-    },
-}
-
 export default new Vuex.Store({
-    state: {
-      todoItems: storage.fetch(),
-        price: 100
-    },
-    getters: {
-        storedTodoItems(state) {
-            return state.todoItems;
-        },
-        originalPrice(state) {
-            return state.price;
-        },
-        doublePrice(state) {
-            return state.price * 2;
-        },
-        triplePrice(state) {
-            return state.price * 3;
-        }
-    },
-    mutations: {
-        addOneItem(state, newTodoItem) {
-            const obj = { completed: false, item: newTodoItem };
-            localStorage.setItem(newTodoItem, JSON.stringify(obj));
-            state.todoItems.push(obj);
-        },
-        removeOneItem(state, payload) {
-            console.log('removeOneItem',payload);
-            localStorage.removeItem(payload.item);
-            state.todoItems.splice(payload.index, 1);
-        },
-        removeAll(state) {
-            localStorage.clear();
-            state.todoItems = [];
-        },
-        complete(state, payload) {
-            console.log(payload);
-            
-            state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
-            localStorage.removeItem(payload.todoItem);
-            localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
-        },
-    },
+    modules: {
+        todoApp
+    }
 })
